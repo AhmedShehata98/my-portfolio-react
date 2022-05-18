83df395758e8f3f0435b741cac0d17dda13ebd5c
Ahmedshehata98 <userstore98@gmail.com>
2022-05-18 12:09:24 +0200

    Development build with files Source - Stage is : under develop alpha build

--- src/components/home/PersonalIMG.jsx
+++ src/components/home/PersonalIMG.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../../scss/pages/home/personalIMG/PersonalIMG.module.css";
-import personalIMGSrc from "../../assets/images/perosnal_img_1.png";
+import personalIMGSrc from "../../assets/images/perosnal_img_1.webp";
 
 const PersonalIMG = () => {
   return (
